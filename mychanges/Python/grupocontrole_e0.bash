6bba7126bcfad4be73f1f61e09df239c172cd4cf
Pedro Henrique Nogueira <nogueirapedro404@gmail.com>
2019-08-30 00:12:08 -0300

    Delete grupocontrole_e0.bash

--- mychanges/Python/grupocontrole_e0.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-find Kepler56_modelo1_e0/Caso*/omega60 -name follow*.out > k56_mod1_omega60_e0.txt
-find Kepler56_modelo1_e0/Caso*/M60 -name follow*.out > k56_mod1_M60_e0.txt
-find Kepler56_modelo2_e0/Caso*/omega60 -name follow*.out > k56_mod2_omega60_e0.txt
-find Kepler56_modelo2_e0/Caso*/M60 -name follow*.out > k56_mod2_M60_e0.txt
-find Kepler-9_2_e0/Caso*/omega60 -name follow*.out > k9_omega60_e0.txt
-find Kepler-9_2_e0/Caso*/M60 -name follow*.out > k9_M60_e0.txt
-
